cf24fa6df81e9b8447ebdb0d3753ac5f87c68fc2
coyove <coyove@hotmail.com>
2017-12-12 19:26:14 +0800

    Test

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
+// LeanEngine 运行时会分配端口并赋值到该变量。
+var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
+
+var http = require('http'), httpProxy = require('./http-proxy.js');
+var proxy = httpProxy.createProxyServer({ changeOrigin: true, ws: true, preserveHeaderKeyCase: true });
+var URL = require('url');
+
+proxy.on
+
+var server = http.createServer(function (req, res) {
+  proxy.web(req, res, {});
+});
+
+server.on('upgrade', function (req, socket, head) {
+  proxy.ws(req, socket, head);
+});
+
+server.listen(PORT);
